ae5ea04e46a630f32dbf8dddc57dfe109033f9bf
Heru Rizky Fajar <herurizkyfajar@gmail.com>
2026-09-20 13:10:39 +0700

    fix: use global dispatch_after_response with backslash

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -102,7 +102,7 @@ class BookingController extends Controller
         ]);
 
         // Operasi lambat dijalankan setelah response dikirim
-        dispatch_after_response(function () use ($booking, $invoice) {
+        \dispatch_after_response(function () use ($booking, $invoice) {
             // Webhook n8n
             try {
                 $booking->load(['vehicle', 'services', 'group']);
